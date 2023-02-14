26210e469cd43a02d3548ab3e4649f71ec3b1955
natopo99 <104119337+natopo99@users.noreply.github.com>
2023-02-13 22:57:16 -0700

    added comments

--- MovieDB/Models/MovieResponse.cs
+++ MovieDB/Models/MovieResponse.cs
@@ -9,19 +9,19 @@ namespace MovieDB.Models
     public class MovieResponse
     {
         [Key]
-        [Required]
+        [Required] //add the required to the fields that need to be submitted to database every time
         public int MovieId { get; set; }
         [Required]
         public string Category { get; set; }
         [Required]
         public string Title { get; set; }
-        [Required]
+        [Required] //
         public int Year { get; set; }
         [Required]
         public string Director { get; set; }
         [Required]
         public string Rating { get; set; }
-
+        //the ones that are not required are Edited, Lento, and Notes
         public bool Edited { get; set; }
         public string LentTo { get; set; }
         [StringLength(25, ErrorMessage = "The field must be less than 25 characters")]
--- MovieDB/Models/NewMovieContext.cs
+++ MovieDB/Models/NewMovieContext.cs
@@ -8,11 +8,12 @@ namespace MovieDB.Models
 {
     public class NewMovieContext : DbContext
     {
+        //call the setup options
         public NewMovieContext (DbContextOptions<NewMovieContext> options) : base(options)
         {
             //leave blank for now
         }
-
+        // get and set the responses that are brough in by the movie response model
         public DbSet<MovieResponse> responses { get; set; }
     }
 }
